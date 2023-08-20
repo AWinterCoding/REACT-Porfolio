22368515b675f93f664f9ea6666231bb20c43931
Alexandra <awinterdrova@gmail.com>
2023-08-20 10:38:59 -0600

    added pictures to the cards

--- react-portfolio/src/components/fragments/ProjectCard.js
+++ react-portfolio/src/components/fragments/ProjectCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 export default function ReturnCards({projects}){
     return(
         <div className="cardHolder">
@@ -7,6 +8,7 @@ export default function ReturnCards({projects}){
           <div className="card">
             <br></br>
             <h3>{`${project.ProjectName}`}</h3>
+            <img src={project.picture} alt="project" className="cardpic"></img>
             <ul className="cardList">
             <li className="cardAnchor">
             <a href={project.GitHub}>GitHub</a>
--- react-portfolio/src/components/fragments/Projects.js
+++ react-portfolio/src/components/fragments/Projects.js
@@ -1,33 +1,49 @@
+import autopassword from "../pages/pageresources/autopasswordgenerator.png";
+import programmingblog from "../pages/pageresources/programmingblog.png";
+import projecthomeroom from "../pages/pageresources/projecthomeroom.png";
+import projectunited from "../pages/pageresources/projectunited.png";
+import readme from "../pages/pageresources/readme.png";
+import weatherapp from "../pages/pageresources/weatherapp.png";
+
 const projects = [
-    {ProjectName: "Project Homeroom",
+    
+{ProjectName: "Project Homeroom",
     GitHub: "https://github.com/AWinterCoding/Project_Home_Room",
 VideoLink: "#",
-DeployedApplication: "https://homeroom-app-4000de1bd278.herokuapp.com/"},
-    {ProjectName: "Project United",
+DeployedApplication: "https://homeroom-app-4000de1bd278.herokuapp.com/",
+picture: projecthomeroom},
+    
+{ProjectName: "Project United",
     GitHub: "https://github.com/Childofrainydays/project-united",
 VideoLink: "#",
-DeployedApplication: "https://project-united.herokuapp.com/"},
-    {ProjectName: "Password Generator",
+DeployedApplication: "https://project-united.herokuapp.com/",
+picture: projectunited},
+    
+{ProjectName: "Password Generator",
          GitHub: "https://github.com/AWinterCoding/Auto-Password-Generator",
           VideoLink: "#",
-           DeployedApplication: "https://awintercoding.github.io/Auto-Password-Generator/"
+           DeployedApplication: "https://awintercoding.github.io/Auto-Password-Generator/",
+           picture: autopassword
         },
-    {ProjectName: "Weather Application",
+    
+{ProjectName: "Weather Application",
     GitHub: "https://github.com/AWinterCoding/WeatherApplication",
 VideoLink: "#",
-DeployedApplication: "https://awintercoding.github.io/WeatherApplication/"},
-{ProjectName: "Simple Logo Generator",
-    GitHub: "https://github.com/AWinterCoding/SimpleLogoGenerator",
-VideoLink: "https://drive.google.com/file/d/1pPlsMVNqX4jyioD-ifAtPilrjMDo-KoH/view",
-DeployedApplication: "#"},
+DeployedApplication: "https://awintercoding.github.io/WeatherApplication/",
+picture: weatherapp},
+
+
 {ProjectName: "README Generator",
     GitHub: "https://github.com/AWinterCoding/README_Generator",
 VideoLink: "#",
-DeployedApplication: "#"},
+DeployedApplication: "#",
+picture:readme},
+
 {ProjectName: "Programming Blog",
     GitHub: "https://github.com/AWinterCoding/ProgrammingBlog",
 VideoLink: "#",
-DeployedApplication: "https://programming-blog-mvc-41e288a7da24.herokuapp.com/"},
+DeployedApplication: "https://programming-blog-mvc-41e288a7da24.herokuapp.com/",
+picture: programmingblog},
 ];
 
 export default projects;
